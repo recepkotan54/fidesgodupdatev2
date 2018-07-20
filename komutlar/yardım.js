c5355885ec77c2eb3dfccd2a1d4320bea110a111
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-20 21:01:49 +0300

    Add files via upload

---
+++ komutlar/yardım.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+  const embedyardim = new Discord.RichEmbed()
+  .setTitle("Komutlar")
+  .setDescription('')
+  .setColor(0x00ffff)
+  .addField("**Eğlence ve Kullanıcı Komutları:**", `\nbf!havadurumu = Yazdığınız Şehrin Hava Durmunu Gösterir. \nbf!nsfw = +18 Resim Gösterir. \nbf!şifre = Yazdığınız Uzunlukta Rastgele Şifreler Oluşturur. \nbf!slots = Slot Makinesinin Kolunu Çekersiniz. \nbf!8ball = Sorduğunuz Sorunun Olup-Olmama İhtimalini Söyler. \nbf!hesapla = Matematik İşlemlerini Yapar. \nbf!emojiyazı = Emoji İle Yazı Yazmanızı Sağlar. \nbf!stresçarkı = Stres Çarkı Döndürürsünüz. \nbf!banned = Dene ve Gör! \nbf!parti = Parti İle İlgili Gif Gösterir! \nbf!avatarım = Avatarınınızı Gösterir. \nbf!yaz = Bota İstediğiniz Birşeyi Yazdırır. \nbf!herkesebendençay = Herkese Çay Alırsınız. \nbf!koş = Koşarsınız.\nbf!çayiç = Çay İçersiniz. \nbf!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nbf!saat = Saati Gösterir. \nbf!çayaşekerat = Çaya Şeker Atarsınız. \nbf!yumruh-at = Yumruk Atarsınız. \nbf!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nbf!sunucuresmi = BOT Sunucunun Resmini Atar. \nbf!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nbf!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
+  .addField("**Yetkilisi Komutlar**", `bf!kur = Sizin Yerinize Gerekli Olan Ses Ve Metin Kanallarını Oluşturur. \nbf!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nbf!kilit = Yazdığınız kanalı belirttiğiniz süreye kadar kilitler. \nbf!temizle = Komutu Yazdığınız Chatteki Mesajları Siler. \nbf!reklamtaraması = Sunucudaki Kişilerin Oynuyor Yerinde Reklam Olup Olmadığını Tespit Eder! \nbf!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nbf!unban = İstediğiniz Kişinin Yasağını Açar. \nbf!sustur = İstediğiniz Kişiyi Susturur. \nbf!oylama = Oylama Açar. \nbf!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
+  .addField("**Ana Komutlar**", "bf!yardım = BOT Komutlarını Atar. \nbf!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nbf!ping = BOT Gecikme Süresini Söyler. \nbf!davet = BOT Davet Linkini Atar. \nbf!istatistik = BOT İstatistiklerini Atar.")
+  .addField("**Yapımcı**", " **Recep | Fides#9190** ")
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(embedyardim);
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bf', 'halp', 'help', 'y'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım [komut]'
+};
